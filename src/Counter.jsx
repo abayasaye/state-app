045f935c3e4de421baddd4dbfe4f34282d052d6e
abayasaye <abay400@gmail.com>
2022-08-11 13:26:30 +0300

    .

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -2,15 +2,35 @@ import {Component} from 'react';
 class Counter extends Component {
     constructor(props){
         super();
-        this.state = {counter : 0};
+        this.state = {counter :props.num};
         this.counterUp = this.counterUp.bind(this)
+        this.counterDown = this.counterDown.bind(this)
+        this.counterRset = this.counterRset.bind(this)
+        this.changeCounterToFive = this.changeCounterToFive.bind(this)
     }
     changeCounterToFive(){
         // this.state.counter == 0 
-        this.setState({counter:5})
+        this.setState({counter: 5})
     }
     counterUp(){
-        this.setState({counter:this.state.counter+1})
+        if (this.state%7===0) {
+            (this.setState({counter:this.state.counter+1}))
+            return(
+                <h1>
+                    BOOM
+                </h1>
+            )
+            }
+        (this.setState({counter:this.state.counter+1}))
+
+    }
+    counterDown(){
+        this.setState({counter:this.state.counter-1})
+        
+    }
+    counterRset(){
+        // eslint-disable-next-line react/no-direct-mutation-state
+        this.setState({counter:this.state.counter=0})
     }
     render(){
         return(
@@ -19,9 +39,24 @@ class Counter extends Component {
                     {this.props.fName}
                 </h5>
                 <button
+                onClick={(this.changeCounterToFive)}
+                >
+                    change Counter To Five
+                </button>
+                <button
                 onClick={(this.counterUp)}
                 >
-                    click
+                    clickUp
+                </button>
+                <button
+                onClick={(this.counterDown)}
+                >
+                    clickDown
+                </button>
+                <button
+                onClick={(this.counterRset)}
+                >
+                Reset
                 </button>
                 <h2>{this.state.counter}</h2>
             </div>
